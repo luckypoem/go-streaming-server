4a96d9f1c37b1e69f76df2e95d6ac557c1e215c0
Xiangrui2019 <xiangrui@aiursoft.com>
2019-07-05 18:00:16 +0800

    Add limiter

--- limiter.go
+++ limiter.go
@@ -1,2 +1,30 @@
 package main
 
+import "log"
+
+type ConnectionLimiter struct {
+	concurrentConnection int
+	bucket               chan int
+}
+
+func NewConnectionLimiter(cc int) *ConnectionLimiter {
+	return &ConnectionLimiter{
+		concurrentConnection: cc,
+		bucket:               make(chan int, cc),
+	}
+}
+
+func (limiter *ConnectionLimiter) GetConnection() bool {
+	if len(limiter.bucket) >= limiter.concurrentConnection {
+		log.Println("Reached the rate limitation.")
+		return false
+	}
+
+	limiter.bucket <- 1
+	return true
+}
+
+func (limiter *ConnectionLimiter) ReleaseConnection() {
+	c := <-limiter.bucket
+	log.Printf("New connection coming %v", c)
+}
