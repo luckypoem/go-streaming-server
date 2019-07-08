bbf6a56941d597ebdbd7ea61cb4f77c3681fcd5b
Xiangrui2019 <xiangrui@aiursoft.com>
2019-07-09 07:17:32 +0800

    add config parser

--- conf/config.go
+++ conf/config.go
@@ -1,2 +1,9 @@
 package conf
 
+type Config struct {
+	Host          string `toml:"HOST"`
+	Port          int    `toml:"PORT"`
+	MaxConnection int    `toml:"MAX_CONNECTION"`
+	VideoDir      string `toml:"VIDEO_DIR"`
+	MaxUploadSize int    `toml"MAX_UPLOAD_SIZE"`
+}
