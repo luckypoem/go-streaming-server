da4b80390fba592b0b252fe1bbdbf2933f44bf2c
Xiangrui2019 <xiangrui@aiursoft.com>
2019-07-08 18:43:44 +0800

    Add params for upload and streaming

--- controllers/videocontroller.go
+++ controllers/videocontroller.go
@@ -1,12 +1,18 @@
 package controllers
 
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
 type VideoController struct {
 }
 
-func (controller *VideoController) Streaming() {
+func (controller *VideoController) Streaming(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 
 }
 
-func (controller *VideoController) Upload() {
+func (controller *VideoController) Upload(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 
 }
